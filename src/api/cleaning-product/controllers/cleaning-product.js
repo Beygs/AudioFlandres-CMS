39e6ae8d3c8f37bb1990e4ec80bdf69c448e95f2
Beygs <boris.gilles@hotmail.fr>
2022-06-21 14:51:17 +0200

    ✨ feat: add cleaning products

---
+++ src/api/cleaning-product/controllers/cleaning-product.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  cleaning-product controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::cleaning-product.cleaning-product');
